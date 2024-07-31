db6dca890a9b67cb80c3a51b2218f5e7ae27eb90
Sandrine Poissonnet <spoisson@gmail.com>
2024-07-31 15:27:04 -0700

    initial set-up for authentication with lucia

---
+++ src/lib/auth.ts
@@ -0,0 +1,79 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import prisma from "./prisma";
+import { Lucia, Session, User } from "lucia";
+import { cache } from "react";
+import { cookies } from "next/headers";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false, // we set the cookie to be valid indefinitely, but we can always destroy the session in our database
+    attributes: {
+      secure: process.env.NODE_ENV === "production", // we only want to use a secure cookie in production mode
+    },
+  },
+  getUserAttributes(databaseUserAttributes) {
+    // return an object with all the fields we want to return to the frontend
+    return {
+      id: databaseUserAttributes.id,
+      username: databaseUserAttributes.username,
+      displayName: databaseUserAttributes.displayName,
+      avatarUrl: databaseUserAttributes.avatarUrl,
+      googleId: databaseUserAttributes.googleId,
+    };
+  },
+});
+
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+  displayName: string;
+  avatarUrl: string | null;
+  googleId: string | null;
+}
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch {}
+
+    return result;
+  },
+);
