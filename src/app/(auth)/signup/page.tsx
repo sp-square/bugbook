ed532f32a51e07838236be26c082f4322176eb48
Sandrine Poissonnet <spoisson@gmail.com>
2024-08-10 16:49:07 -0700

    signup page in progress

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,40 @@
+// this page is a server component - this is why we can set the metadata in here
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import signupImage from "@/assets/signup-image.jpg";
+import SignUpForm from "./SignUpForm";
+
+export const metadata: Metadata = {
+  title: "Sign Up",
+};
+
+export default function Page() {
+  return (
+    <main className="flex h-screen items-center justify-center p-5">
+      <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <div className="space-y-1 text-center">
+            <h1 className="text-3xl font-bold">Sign up to bugbook</h1>
+            <p className="text-muted-foreground">
+              A place where even <span className="italic">you</span> can find a
+              friend.
+            </p>
+          </div>
+          <div className="space-y-5">
+            {/* since we don't have interactivity on the server side, we need to import a client component for the signup form, where we can set-up javascript interactivity with the user */}
+            <SignUpForm />
+            <Link href="/login" className="block text-center hover:underline">
+              Already have an account? Log in
+            </Link>
+          </div>
+        </div>
+        <Image
+          src={signupImage}
+          alt=""
+          className="hidden w-1/2 object-cover md:block"
+        />
+      </div>
+    </main>
+  );
+}
